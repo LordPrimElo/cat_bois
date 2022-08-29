c9aaa6dd9e385fba0aa7928e5d892c9cdca770ec
LordPrimElo <evansrathore@gmail.com>
2022-08-29 19:53:25 +0530

    Cat bois

---
+++ cat.js
@@ -0,0 +1,18 @@
+let timeLapsed = 0
+
+const getCats = async () => {
+    const hehe = "https://api.giphy.com/v1/gifs/search?api_key=tqmcrWoXD2roOq2AAHF0hSWLB1NaezVi&q=cat&limit=5&offset=" + timeLapsed
+    const cats = await fetch(hehe).then(res => res.json())
+    cats.data.forEach(cat => {
+        let cat_div = document.createElement("img")
+        cat_div.src = cat.images.fixed_width.url
+
+        document.body.appendChild(cat_div)
+    });
+    timeLapsed = timeLapsed+=5
+}
+
+const btn = document.getElementById("cat-butt")
+btn.onclick = getCats
+
+
