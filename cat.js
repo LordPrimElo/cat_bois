3a47274548c766be4f076e6eb6e5f61b4f455931
Evan Rathore <87514160+LordPrimElo@users.noreply.github.com>
2022-08-29 15:18:07 +0000

    Update cat.js

--- cat.js
+++ cat.js
@@ -1,7 +1,7 @@
 let timeLapsed = 0
 
 const getCats = async () => {
-    const hehe = "https://api.giphy.com/v1/gifs/search?api_key=tqmcrWoXD2roOq2AAHF0hSWLB1NaezVi&q=bodybuilding&limit=5&offset=" + timeLapsed
+    const hehe = "https://api.giphy.com/v1/gifs/search?api_key=tqmcrWoXD2roOq2AAHF0hSWLB1NaezVi&q=cat&limit=5&offset=" + timeLapsed
     const cats = await fetch(hehe).then(res => res.json())
     cats.data.forEach(cat => {
         let cat_div = document.createElement("img")
